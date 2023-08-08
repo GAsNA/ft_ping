2cbf03511e948a1f7fe1d5fb63de86c08f8ad9e4
GAsNa <romane.leseur.pro@gmail.com>
2023-08-08 19:08:07 +0200

    no destination address is good

--- src/main.c
+++ src/main.c
@@ -2,9 +2,14 @@
 
 #include "../libft/libft.h"
 
+void	no_destination_address(void)
+{
+	printf("ft_ping: usage error: Destination address required\n");
+}
+
 void	help(void)
 {
-	printf("Usage\n");
+	printf("\nUsage\n");
 	printf("\tft_ping [options] <destination>\n");
 	printf("\n");
 	printf("Options:\n");
@@ -15,24 +20,35 @@ void	help(void)
 
 void	unknown_argument(char *arg)
 {
-	printf("ft_ping: invalid argument: \"%s\"\n\n", arg);
+	printf("ft_ping: invalid argument: \"%s\"\n", arg);
 }
 
 int	main(int ac, char **av)
 {
 	if (ac < 2)
 	{
-		printf("ft_ping: usage error: Destination address required\n");
+		no_destination_address();
 		return 1;
 	}
+
+	int	verbose = 0;
+	int	addr = 0;
 
 	for(int i = 1; i < ac; i++)
 	{
 		if (!ft_strcmp(av[i], "-h")) { help(); return 0; }
-		if (!ft_strcmp(av[i], "-v")) { /*verbose*/ }
+		else if (!ft_strcmp(av[i], "-v")) { verbose = 1; }
 		else if (av[i][0] == '-') { unknown_argument(av[i]); help(); return 1; }
-		else { /*argument is to ping*/ }
+		else { addr = 1; /*argument is to ping*/ }
 	}
+
+	if (!addr)
+	{
+		no_destination_address();
+		return 1;
+	}
+
+	(void)verbose;
 
 	return 0;
 }
