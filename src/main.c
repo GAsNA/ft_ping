2d1e2fdfa22064402ec19ae8d3ef82893311cbf0
GAsNa <romane.leseur.pro@gmail.com>
2023-08-21 12:48:38 +0200

    create structure ft_ping

--- src/main.c
+++ src/main.c
@@ -10,6 +10,19 @@
 
 #include "../libft/libft.h"
 
+typedef struct s_ft_ping	t_ft_ping;
+
+struct s_ft_ping
+{
+	struct addrinfo	*addrinfo;
+	int				socket_fd;
+	char			*addr;
+	char			ip[INET_ADDRSTRLEN];
+	int				id;
+};
+
+t_ft_ping	g_ping;
+
 void	no_destination_address(void)
 {
 	printf("ft_ping: usage error: Destination address required\n");
@@ -36,14 +49,11 @@ void	unknown_name_service(char *name)
 	printf("ft_ping: %s: Name or service not known\n", name);
 }
 
-struct addrinfo	*result;
-int				socket_fd;
-
 void	stop(int sig)
 {
 	(void)sig;
-	close(socket_fd);
-	freeaddrinfo(result);
+	close(g_ping.socket_fd);
+	freeaddrinfo(g_ping.addrinfo);
 	exit(0);
 }
 
@@ -71,43 +81,41 @@ int	main(int ac, char **av)
 	}
 
 	int	verbose = 0;
-	char *addr;
 
 	for(int i = 1; i < ac; i++)
 	{
 		if (!ft_strcmp(av[i], "-h")) { help(); return 0; }
 		else if (!ft_strcmp(av[i], "-v")) { verbose = 1; }
 		else if (av[i][0] == '-') { unknown_argument(av[i]); help(); return 1; }
-		else { addr = av[i]; }
+		else { g_ping.addr = av[i]; }
 	}
 
-	if (!addr) { no_destination_address(); return 1; }
+	if (!g_ping.addr) { no_destination_address(); return 1; }
 
 	// GET INFO FROM ADDRESS
-	if (getaddrinfo(addr, NULL, NULL, &result)) { unknown_name_service(addr); return 1; }
+	if (getaddrinfo(g_ping.addr, NULL, NULL, &g_ping.addrinfo)) { unknown_name_service(g_ping.addr); return 1; }
 
 	// SOCKET
-	socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-	if (socket_fd < 0) { printf("ft_ping: error: creating socket failed\n"); return 1; }
+	g_ping.socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (g_ping.socket_fd < 0) { printf("ft_ping: error: creating socket failed\n"); return 1; }
 
 	// SETSOCKOPT
 	struct timeval	timeout;
 	timeout.tv_sec = 1;
 	timeout.tv_usec = 0;
-	if (setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout)) == -1) {
+	if (setsockopt(g_ping.socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout)) == -1) {
 		printf("ft_ping: error: setsockopt failed\n"); return 1; }
 
 	// GET IP
-	struct sockaddr_in *dest = (struct sockaddr_in *)result->ai_addr;
-	char ip[INET_ADDRSTRLEN];
-	inet_ntop(AF_INET, &(dest->sin_addr), ip, INET_ADDRSTRLEN);
+	struct sockaddr_in *dest = (struct sockaddr_in *)g_ping.addrinfo->ai_addr;
+	inet_ntop(AF_INET, &(dest->sin_addr), g_ping.ip, INET_ADDRSTRLEN);
 
-	int id = getpid();
+	g_ping.id = getpid();
 
 	// FIRST INFORMATION
-	printf("FT_PING %s (%s): %d data bytes", addr, ip, 56); // TODO what is 56 ?
+	printf("FT_PING %s (%s): %d data bytes", g_ping.addr, g_ping.ip, 56); // TODO what is 56 ?
 	if (verbose)
-		printf(", id 0x%x = %d", id, id);
+		printf(", id 0x%x = %d", g_ping.id, g_ping.id);
 	printf("\n");
 
 	// HANDLING SIGNAL
@@ -121,7 +129,7 @@ int	main(int ac, char **av)
 	icmp.type = ICMP_ECHO;
 	icmp.code = 0;
 	icmp.checksum = 0;
-	icmp.un.echo.id = id;
+	icmp.un.echo.id = g_ping.id;
 	icmp.un.echo.sequence = 0;
 	//icmp.un.gateway
 	//icmp.un.frag.__unused
@@ -134,9 +142,10 @@ int	main(int ac, char **av)
 		icmp.un.echo.sequence++;
 		
 		// SEND PACKET
-		ssize_t	res = sendto(socket_fd, &icmp, sizeof(icmp), 0, result->ai_addr, result->ai_addrlen);
+		ssize_t	res = sendto(g_ping.socket_fd, &icmp, sizeof(icmp), 0, g_ping.addrinfo->ai_addr, g_ping.addrinfo->ai_addrlen);
 		if (res == sizeof(icmp)) { printf("SEND %d\n", icmp.un.echo.sequence); }
 
+		//TODO RECEIVE PACKET
 		// WAIT TILL TOTAL OF LOOP IS 1 SECOND
 		do { gettimeofday(&end, NULL); } while (end.tv_sec - begin.tv_sec < 1);
 	}
